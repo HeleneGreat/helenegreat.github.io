1ac34e100b8ab2fce2df31f5bfed358ad9a70450
HeleneGreat <helene.dev@yahoo.com>
2024-03-27 16:45:40 +0100

    Header responsive

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,8 @@
 import { NavLink, Link } from 'react-router-dom'
+import { useState } from 'react'
 import styled from 'styled-components'
 import colors from '../../utils/colors'
+import { ReactComponent as MenuBurger } from '../../assets/menu-burger.svg'
 import { breakpoints } from '../../utils/css-breakpoints'
 import fonts from '../../utils/fonts'
 
@@ -16,8 +18,46 @@ const StyledContainer = styled('div')`
     display: flex;
     justify-content: space-between;
     align-items: center;
+    @media screen and ${breakpoints.tablet} {
+    }
 `
-const afterStyle = `
+const StyledNav = styled('nav')`
+    @media screen and (max-width: 768px) {
+        background-color: ${colors.primary};
+        transition: all 0.3s ease-in;
+        opacity: 0;
+        visibility: hidden;
+        width: 0;
+        position: absolute;
+        top: 110px;
+        right: 0;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        height: calc(100vh - 110px);
+        &.nav-active {
+            width: 70%;
+            opacity: 1;
+            visibility: visible;
+        }
+    }
+`
+const StyledMenuBurger = styled(MenuBurger)`
+    flex-grow: 1;
+    transform: scaleX(-1);
+    path {
+        stroke: #fff;
+    }
+`
+const StyledMenuICon = styled('div')`
+    cursor: pointer;
+    flex-grow: 1;
+    text-align: right;
+    @media screen and ${breakpoints.tablet} {
+        display: none;
+    }
+`
+const afterLinkStyle = `
     content: '';
     position: absolute;
     bottom: 0;
@@ -26,8 +66,11 @@ const afterStyle = `
     height: 7px;
     background-color: ${colors.tertiary};
 `
-
 const StyledLink = styled(NavLink)`
+    @media screen and (max-width: 768px) {
+        width: fit-content;
+        margin-bottom: 20px;
+    }
     padding: 15px;
     margin: 0 10px;
     font-size: 25px;
@@ -37,17 +80,22 @@ const StyledLink = styled(NavLink)`
     position: relative;
     &.active {
         &:after {
-            ${afterStyle}
+            ${afterLinkStyle}
         }
     }
     &:hover {
         &:after {
-            ${afterStyle}
+            ${afterLinkStyle}
         }
     }
 `
 
 function Header() {
+    const [showNavbar, setShowNavbar] = useState(false)
+    const handleShowNavbar = () => {
+        setShowNavbar(!showNavbar)
+    }
+
     return (
         <StyledHeader>
             <StyledContainer className="container">
@@ -61,12 +109,15 @@ function Header() {
                         />
                     </Link>
                 </div>
-                <nav>
+                <StyledMenuICon onClick={handleShowNavbar}>
+                    <StyledMenuBurger width={100} height={100} />
+                </StyledMenuICon>
+                <StyledNav className={`${showNavbar && 'nav-active'}`}>
                     <StyledLink to="/">Accueil</StyledLink>
                     <StyledLink to="/parcours">Parcours</StyledLink>
                     <StyledLink to="/portfolio">Portfolio</StyledLink>
                     <StyledLink to="/contact">Contact</StyledLink>
-                </nav>
+                </StyledNav>
             </StyledContainer>
         </StyledHeader>
     )
