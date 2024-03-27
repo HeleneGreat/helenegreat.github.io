162d44a2d7ef78b9a701351c2763e7412ff7e9d1
HeleneGreat <helene.dev@yahoo.com>
2024-03-27 16:45:18 +0100

    Footer responsive

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -4,6 +4,7 @@ import colors from '../../utils/colors'
 import fonts from '../../utils/fonts'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSquareGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons'
+import { breakpoints } from '../../utils/css-breakpoints'
 
 const StyleFooter = styled('footer')`
     padding: 30px 0;
@@ -11,25 +12,44 @@ const StyleFooter = styled('footer')`
     color: #fff;
 `
 const StyledContainer = styled('div')`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
+    @media screen and ${breakpoints.tablet} {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+    }
 `
 const StyledName = styled('div')`
     p {
         font-family: ${fonts.colombo};
         &.name {
-            font-size: 55px;
+            font-size: 30px;
+            @media screen and ${breakpoints.tablet} {
+                font-size: 45px;
+            }
+            @media screen and ${breakpoints.laptop} {
+                font-size: 55px;
+            }
         }
         &.job {
-            font-size: 35px;
+            font-size: 20px;
+            @media screen and ${breakpoints.tablet} {
+                font-size: 28px;
+            }
+            @media screen and ${breakpoints.laptop} {
+                font-size: 35px;
+            }
         }
     }
 `
-
 const StyledLinks = styled('div')`
     a:not(:first-of-type) {
-        margin-left: 50px;
+        margin-left: 20px;
+        @media screen and ${breakpoints.tablet} {
+            margin-left: 20px;
+        }
+        @media screen and ${breakpoints.laptop} {
+            margin-left: 50px;
+        }
     }
     svg {
         transition: all 0.4s ease-in-out;
@@ -40,7 +60,10 @@ const StyledLinks = styled('div')`
 `
 const StyledFontAwesomeIcon = styled(FontAwesomeIcon)`
     color: #fff;
-    font-size: 50px;
+    font-size: 40px;
+    @media screen and ${breakpoints.tablet} {
+        font-size: 50px;
+    }
 `
 
 function Footer() {
