458eb5e4ae915a414c2ffe44894255a24fcfaf34
HeleneGreat <helene.dev@yahoo.com>
2024-03-28 12:30:02 +0100

    Page erreur responsive

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,8 +1,41 @@
+import styled from 'styled-components'
+import colors from '../../utils/colors'
+import errorImg from '../../assets/404-error.svg'
+import { breakpoints } from '../../utils/css-breakpoints'
+
+const StyledError = styled('div')`
+    background-color: ${colors.secondary};
+    text-align: center;
+    h1 {
+        padding: 50px 0;
+        font-size: 30px;
+        @media screen and ${breakpoints.tablet} {
+            font-size: 55px;
+        }
+    }
+`
+const StyledImg = styled('img')`
+    width: 100%;
+    height: auto;
+    margin-bottom: 30px;
+    @media screen and ${breakpoints.tablet} {
+        width: 70%;
+    }
+`
+
 function Error() {
     return (
-        <div className="main container">
-            <h1>Oups 🙈 Cette page n'existe pas</h1>
-        </div>
+        <StyledError className="main">
+            <div className="container">
+                <h1>Oups, cette page n'existe pas</h1>
+                <StyledImg
+                    src={errorImg}
+                    alt="Pas non trouvée"
+                    width={800}
+                    height={530}
+                />
+            </div>
+        </StyledError>
     )
 }
 
