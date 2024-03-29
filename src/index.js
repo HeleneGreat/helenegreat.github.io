2d92c774f203348b829722218cb0254604532183
HeleneGreat <helene.dev@yahoo.com>
2024-03-29 15:45:20 +0100

    404 router hash

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { HashRouter  as Router, Routes, Route } from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
